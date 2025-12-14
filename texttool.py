7c7c9acd46cb3735b30db2797c55aa8736d15892
Sonia Boutaleb <soniaboutaleb@MacBook-Air-de-Sonia.local>
2025-12-14 10:13:13 +0100

    Ajout de la commande count-words

--- texttool.py
+++ texttool.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python3
-
-
-
 def process_line(line):
     if " " not in line:
         return "No command or no argument given"
-
     cmd, text = line.split(" ", maxsplit=1)
-
     if cmd == "uppercase":
         return text.upper()
     if cmd == "lowercase":
         return text.lower()
-
+    if cmd == "count-words":
+        return len(text.split())
     return "Unknown command " + cmd
-
-
-
 def main():
     while True:
         try:
             line = input("commade> ")
         except EOFError:
             break
-
         print(process_line(line))
-
-
-
 if __name__ == "__main__":
     main()
